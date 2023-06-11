bfcf0584fe94afe819f3b13758606e8a8e234275
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-11 16:25:28 -0700

    Connecting Our Components

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
 import Navigation from './components/Navigation'
 import Profile from './components/Profile'
 import TweetList from './components/TweetList'
 import TweetForm from './components/TweetForm'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <Profile />
+      <TweetList />
+      <TweetForm />
     </div>
   );
 }

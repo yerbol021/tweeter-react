1f79efffe6f7356cc8955929b9788a28bf87c5a4
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-11 20:20:21 -0700

    Handling Events In React

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,16 +1,37 @@
-import React from 'react'; //optional
+import React from 'react';
 
-function TweetForm(){
-  const message = "What are you humming about?"
+function TweetForm() {
+  const message = "What are you humming about?";
+
+  const handleClick = (event) => {
+    event.preventDefault();
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Additional logic for form submission if needed
+  };
+
   return (
     <section className="newtweet">
-    <form method="post" action="/tweets" className="newtweet__form">
-      <textarea className="form__textarea" name="text" placeholder={message}></textarea>
-      <input type="submit" value="Tweet" className="form__input"/>
-      <span className="form__counter">140</span>
-    </form>
-  </section>
-
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={handleSubmit}>
+        <textarea className="form__textarea" name="text" placeholder={message}></textarea>
+        <input
+          type="submit"
+          value="Tweet"
+          className="form__input"
+          onClick={handleClick}
+          onKeyDown={handleKeyDown}
+        />
+        <span className="form__counter">140</span>
+      </form>
+    </section>
   );
 }
 
